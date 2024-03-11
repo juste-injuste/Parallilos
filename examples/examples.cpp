ccb2aa9d5e9ee121a73608bd3d5d39f23dc91bfa
juste-injuste <79662795+juste-injuste@users.noreply.github.com>
2024-03-10 20:08:20 -0400

    i dont know why there is no speedup :(

--- examples/examples.cpp
+++ examples/examples.cpp
@@ -4,6 +4,7 @@
 
 // #define PPZ_DEBUGGING
 #include "Parallilos.hpp"
+#include "Chronometro.hpp"
 
 template<typename T> inline
 void print_array(const ppz::Array<T>& array)
@@ -41,39 +42,55 @@ int main()
   {
     c[k] = a[k] * b[k];
   }
+
+  CHZ_MEASURE(10, "iteration %# took %us")
+  CHZ_LOOP_FOR(1000000)
+  {
+    for (const auto k : ppz::SIMD<type>::parallel(n))
+    {
+      ppz::simd_storea(c+k, ppz::simd_mul(a.as_vector(k), b.as_vector(k)));
+    }
+
+    for (const auto k : ppz::SIMD<type>::sequential(n))
+    {
+      c[k] = a[k] * b[k];
+    }
+  }
+  std::cout << "Instruction set: " << ppz::SIMD<type>::set  << '\n';
+
   
-  // display result array
-  print_array(c);
+  // // display result array
+  // print_array(c);
 
-  auto v1 = ppz::simd_setval<double>(1);
-  auto v2 = ppz::simd_setval<char>('2');
-  auto v3 = ppz::simd_setval<int32_t>(3);
-  auto v4 = ppz::simd_setval<float>(4);
+  // auto v1 = ppz::simd_setval<double>(1);
+  // auto v2 = ppz::simd_setval<char>('2');
+  // auto v3 = ppz::simd_setval<int32_t>(3);
+  // auto v4 = ppz::simd_setval<float>(4);
 
-  auto m1 = ppz::simd_gt(v1, v1);
-  auto m2 = ppz::simd_gt(v2, v2);
-  auto m3 = ppz::simd_gt(v3, v3);
-  auto m4 = ppz::simd_gt(v4, v4);
+  // auto m1 = ppz::simd_gt(v1, v1);
+  // auto m2 = ppz::simd_gt(v2, v2);
+  // auto m3 = ppz::simd_gt(v3, v3);
+  // auto m4 = ppz::simd_gt(v4, v4);
 
-  std::cout << typeid(decltype(m1)).name() << '\n';
-  std::cout << typeid(decltype(m2)).name() << '\n';
-  std::cout << typeid(decltype(m3)).name() << '\n';
-  std::cout << typeid(decltype(m4)).name() << '\n';
+  // std::cout << typeid(decltype(m1)).name() << '\n';
+  // std::cout << typeid(decltype(m2)).name() << '\n';
+  // std::cout << typeid(decltype(m3)).name() << '\n';
+  // std::cout << typeid(decltype(m4)).name() << '\n';
 
-  std::cout << "double: " << v1 << '\n';
-  std::cout << "char:   " << v2 << '\n';
-  std::cout << "int32:  " << v3 << '\n';
-  std::cout << "float:  " << v4 << '\n';
+  // std::cout << "double: " << v1 << '\n';
+  // std::cout << "char:   " << v2 << '\n';
+  // std::cout << "int32:  " << v3 << '\n';
+  // std::cout << "float:  " << v4 << '\n';
 
-  auto a1 = ppz::simd_setval(1.0f);
-  auto b1 = ppz::simd_setval(2.0f);
-  std::cout << ppz::simd_add(a1, b1) << '\n'; // prints " 3   3   ...  3"
-  std::cout << ppz::simd_sub(a1, b1) << '\n'; // prints "-1  -1   ... -1"
-  std::cout << ppz::simd_mul(a1, b1) << '\n'; // prints " 2   2   ...  2"
-  std::cout << ppz::simd_div(a1, b1) << '\n'; // prints " 0.5 0.5 ...  0.5"
+  // auto a1 = ppz::simd_setval(1.0f);
+  // auto b1 = ppz::simd_setval(2.0f);
+  // std::cout << ppz::simd_add(a1, b1) << '\n'; // prints " 3   3   ...  3"
+  // std::cout << ppz::simd_sub(a1, b1) << '\n'; // prints "-1  -1   ... -1"
+  // std::cout << ppz::simd_mul(a1, b1) << '\n'; // prints " 2   2   ...  2"
+  // std::cout << ppz::simd_div(a1, b1) << '\n'; // prints " 0.5 0.5 ...  0.5"
 
-  auto a2 = ppz::simd_setval(2);
-  auto b2 = ppz::simd_setval(-2);
-  std::cout << a2 << " -> " << ppz::simd_abs(a2) << '\n';
-  std::cout << b2 << " -> " << ppz::simd_abs(b2) << '\n';
+  // auto a2 = ppz::simd_setval(2);
+  // auto b2 = ppz::simd_setval(-2);
+  // std::cout << a2 << " -> " << ppz::simd_abs(a2) << '\n';
+  // std::cout << b2 << " -> " << ppz::simd_abs(b2) << '\n';
 }
